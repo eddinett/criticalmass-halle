c0475289c4805dc3ae9f8db71c24e5f497ff317c
Eddi <82827440+eddinett@users.noreply.github.com>
2021-12-05 18:27:50 +0100

    Add file in v.3.3.1

---
+++ scripts/panel.js
@@ -0,0 +1,17 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+// document updated in version 3.2.1 from 2021/11/29
+// CODED BY EDDINETT.DEV
